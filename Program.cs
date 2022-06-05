e84455a9a149b6531ee0a2e8c700dd86c6781b67
KessyRamos <kessyellenramos@gmail.com>
2022-06-05 09:43:00 -0300

    PrimeiroCommit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿double f = 86;
+double c = (f - 32) * 5 / 9;
+
+Console.WriteLine("Calcular a conversão da temperatura em graus Celsius!!!");
+
+Console.Write($"A conversão de Fahrenheit para Celsius é: {c}°");
